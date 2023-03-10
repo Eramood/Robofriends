73b5630786e69f8434c27636f6cd303b16db01c7
Eramood <122992858+Eramood@users.noreply.github.com>
2023-03-10 15:42:30 +0100

    Added a searchbox

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Cardlist from './Cardlist';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 import 'tachyons'
-import  {robots}  from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Cardlist robots={robots}/>
+  <App/>
 );
 reportWebVitals();
